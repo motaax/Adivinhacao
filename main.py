0d2ec5d1e204f24691ea0a88ae25acfbbc0f7062
Felipe Mota <86206681+motaax@users.noreply.github.com>
2024-11-12 22:50:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('Vamos jogar um jogo de adivinhação. Tente adivinhar o número que estou pensando entre 1 e 10.')
 
 import random 
-numero_secreto = random.randit(1, 10)
+numero_secreto = random.randint(1, 10)
 tentativa = None
 
 while tentativa != numero_secreto:
