bc8fd3ba64a9f3b8bb10eb88fdc3504040ded032
Felipe Mota <felipemota465@gmail.com>
2024-11-12 22:40:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Vamos jogar um jogo de adivinhação. Tente adivinhar o número que estou pensando entre 1 e 10.')
+
+import random 
+numero_secreto = random.randit(1, 10)
+tentativa = None
+
+while tentativa != numero_secreto:
+    tentativa = int(input('Digite o seu palpite: '))
+    if tentativa < numero_secreto:
+        print('Mais alto!')
+    elif tentativa > numero_secreto:
+        print('Mais baixo!')
+    
+print('Parabéns! Você adivinhou o número.')
